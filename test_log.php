07e2a305c8ba0c30dc228b2329189320c2a8851f
LucaGoubelle <55276685+LucaGoubelle@users.noreply.github.com>
2021-05-30 13:46:42 +0200

    Delete test_log.php

--- test_log.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	require "src/controllers/dataPretreatmentService/csvTSDataManager.php";
-
-	$csvArr = CSV_TS_Data_Manager::arrayFromCSV("src/data/__matrix_form_407pl.csv");
-	var_dump($csvArr);
-
-?>
